d01f814e4492f63ca63203037121a770ac605830
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2017-10-10 11:20:12 +0100

    problem 5

---
+++ 5. Smallest multiple/index.ts
@@ -0,0 +1,19 @@
+import { $isFactorial } from '../shared';
+
+(function() {
+    const maxNumber = 20;
+
+    let start: number = maxNumber - 2;
+
+    while (!checkFactors(++start * maxNumber, maxNumber)) {}
+
+    console.log(start * maxNumber);
+
+    function checkFactors(value: number, factor: number): boolean {
+        if (factor === 1) {
+            return true;
+        }
+
+        return $isFactorial(value, factor) && checkFactors(value, --factor);
+    }
+})();
