80a37cb870a473f6f7b38ed4919528fbffb1d7f5
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2017-10-10 13:32:41 +0100

    problem 7

---
+++ 7. 10001st prime/index.ts
@@ -0,0 +1,20 @@
+import { $isPrime } from '../shared/is-prime';
+
+(function() {
+    const index = 10001;
+
+    let primeCounter: number = 1;
+    let lastPrime: number = 2;
+    let testNumber: number = 1;
+
+    while (primeCounter < index) {
+        testNumber += 2;
+
+        if ($isPrime(testNumber)) {
+            lastPrime = testNumber;
+            primeCounter++;
+        }
+    }
+
+    console.log(lastPrime);
+})();
---
+++ shared/is-prime.ts
@@ -0,0 +1,14 @@
+/** @private **/
+import { $isInteger } from './is-integer';
+
+export function $isPrime(a: number): boolean {
+    for (let factor: number = 2, value: number = a; factor <= value; factor++) {
+        value = a / factor;
+
+        if (value !== 1 && $isInteger(value)) {
+            return false;
+        }
+    }
+
+    return true;
+}
