2e741aed4b73e37e94ce2a26073b1fbf149e41f4
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2017-10-12 15:51:46 +0100

    problem 16 update

--- 16. Power digit sum/index.ts
+++ 16. Power digit sum/index.ts
@@ -1,28 +1,16 @@
-import { $perf } from '../shared';
+import { $BigNum, $perf } from '../shared';
 
 $perf(() => {
     const power = 1000;
     const base = 2;
 
-    const digitArray: Array<number> = [1];
+    const num = new $BigNum();
 
     for (let x = 0; x < power; x++) {
-        const finalCarry: number = digitArray.reduce<number>((carry: number, digit: number, index: number) => {
-            const product = (digit * base) + carry;
-
-            const [newCarry, newDigit] = (product / 10).toString().split('.');
-
-            digitArray[index] = parseInt(newDigit || '0', 10);
-
-            return parseInt(newCarry || '0', 10);
-        }, 0);
-
-        if (finalCarry) {
-            digitArray.push(finalCarry);
-        }
+        num.multiply(base);
     }
 
-    console.log(digitArray.reduce((total: number, digit: number) => {
+    console.log(num.digitArray.reduce((total: number, digit: number) => {
         return total + digit;
     }, 0));
 });
