83b25ecd0e7aa9772e20efe0f76e4561db84a15b
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-09-13 10:24:18 +0100

    IsPrime should only account for moduloed numbers

--- shared/is-prime.ts
+++ shared/is-prime.ts
@@ -2,6 +2,7 @@
 import { $isInteger } from './is-integer';
 
 export function $isPrime(a: number): boolean {
+    a = Math.abs(a);
     for (let factor: number = 2, value: number = a; factor <= value; factor++) {
         value = a / factor;
 
