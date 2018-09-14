704de91f395a2003e77ea68518c2eaac2f64bc29
Anthony Cleaver <anthony.cleaver@mhr.co.uk>
2018-09-14 15:15:07 +0100

    problem 40

---
+++ 40. Champernowne's constant/index.ts
@@ -0,0 +1,21 @@
+import { $perf } from '../shared';
+
+$perf(() => {
+    const limit = 1e6;
+    let concat: string = '.';
+    let x: number = 0;
+
+    while (concat.length <= limit) {
+        concat += ++x;
+    }
+
+    let index = 1;
+    let product = 1;
+
+    while (index <= limit) {
+        product *= parseInt(concat[index]);
+        index *= 10;
+    }
+
+    console.log(product);
+});
